7f82395411ad826e76d8053a06bd867eb45040ae
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-04-01 22:24:54 +0300

    refactor(gulp): disable scss cache

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -6,26 +6,28 @@ import tailwindcss from '@tailwindcss/postcss';
 import postcss from 'gulp-postcss';
 import cssnano from 'cssnano';
 import clone from 'gulp-clone';
-import cache from 'gulp-cached';
+// import cache from 'gulp-cached';
 
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-	return app.gulp
-		.src(app.path.src.scss)
-		.pipe(cache('scss'))
-		.pipe(
-			app.plugins.plumber(
-				app.plugins.notify.onError({
-					title: 'SCSS',
-					message: 'Error: <%= error.message %>',
-				}),
-			),
-		)
-		.pipe(sass({ style: 'expanded' }))
-		.pipe(postcss([tailwindcss(), autoprefixer({ grid: 'autoplace' })]))
-		.pipe(clone())
-		.pipe(postcss([cssnano({ preset: 'default' })]))
-		.pipe(rename({ extname: '.min.css' }))
-		.pipe(app.gulp.dest(app.path.build.css));
+	return (
+		app.gulp
+			.src(app.path.src.scss)
+			// .pipe(cache('scss'))
+			.pipe(
+				app.plugins.plumber(
+					app.plugins.notify.onError({
+						title: 'SCSS',
+						message: 'Error: <%= error.message %>',
+					}),
+				),
+			)
+			.pipe(sass({ style: 'expanded' }))
+			.pipe(postcss([tailwindcss(), autoprefixer({ grid: 'autoplace' })]))
+			.pipe(clone())
+			.pipe(postcss([cssnano({ preset: 'default' })]))
+			.pipe(rename({ extname: '.min.css' }))
+			.pipe(app.gulp.dest(app.path.build.css))
+	);
 };
