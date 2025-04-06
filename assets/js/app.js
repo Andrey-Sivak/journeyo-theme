9155612510a4ad84d88cae915f00117353798c7d
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-04-06 21:46:46 +0300

    refactor: move slider JS to separate module

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,9 @@
 		),
 		header: document.querySelector('.wp-block-journeyo-footer'),
 		form: document.querySelector('.wpcf7-form'),
+		reviewsSection: document.querySelector(
+			'.wp-block-journeyo-reviews-section',
+		),
 	};
 
 	if (elements.header) {
@@ -13,6 +16,12 @@
 
 	if (elements.form) {
 		import('./Form.js').then(({ default: Form }) => new Form());
+	}
+
+	if (elements.reviewsSection) {
+		import('./ReviewsSlider.js').then(
+			({ default: ReviewsSlider }) => new ReviewsSlider(),
+		);
 	}
 
 	if (elements.footer) {
