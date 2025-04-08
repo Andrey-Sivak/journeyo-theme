3d1d10a0af170137373ab334d11240386edf221f
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-04-08 13:53:49 +0300

    build: version up

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
  * The journeyo functions and definitions
  */
 
-define('_JN_VERSION', '1.0.26');
+define('_JN_VERSION', '1.0.3');
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
